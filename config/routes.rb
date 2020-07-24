79cc07c90032709c2232667a22181f1490e2a674
Justin Langlinais <justinclanglinais@gmail.com>
2020-07-24 10:32:02 -0700

    added topic and category index and routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
     end
   end
   resources :sessions, only: [:index, :show, :update, :edit]
+  get '/topics', to: 'topics#index'
+  get '/categories', to: 'categories#index'
 end
