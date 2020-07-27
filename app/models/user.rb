05ad60f1d9c67a282ae907f7c4d64408c633062a
Justin Langlinais <justinclanglinais@gmail.com>
2020-07-27 16:11:49 -0700

    class index and class list

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     has_many :sessions, through: :enrollments
     has_secure_password
     accepts_nested_attributes_for :sessions
-    # validates :email, uniqueness: { case_sensitive: false }
+    validates :email, uniqueness: { case_sensitive: false }
     def self.instructors
         User.all.select{|u| u.isAdmin}
     end
