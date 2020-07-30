491bbee73cbb7c29a674a3b042509b1e70c40f8d
Justin Langlinais <justinclanglinais@gmail.com>
2020-07-30 14:13:47 -0700

    trying default value for imgurl

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
     has_secure_password
     accepts_nested_attributes_for :sessions
     validates :email, uniqueness: { case_sensitive: false }
+
     def self.instructors
         User.all.select{|u| u.isAdmin}
     end
